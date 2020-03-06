6d5fe0d94b6776f64b540179aa231a1380c7d623
Steven McLeod <steven@mcleodnet.com>
2020-03-06 01:03:50 -0800

    Sprite update

--- dkong_system.srcs/sources_1/bd/dkong/ip/dkong_framedoubler_slow_0_0/dkong_framedoubler_slow_0_0_sim_netlist.v
+++ dkong_system.srcs/sources_1/bd/dkong/ip/dkong_framedoubler_slow_0_0/dkong_framedoubler_slow_0_0_sim_netlist.v
@@ -1,10 +1,10 @@
 // Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 // --------------------------------------------------------------------------------
 // Tool Version: Vivado v.2019.1 (win64) Build 2552052 Fri May 24 14:49:42 MDT 2019
-// Date        : Fri Feb 28 16:40:46 2020
-// Host        : ensc-pit-w26 running 64-bit major release  (build 9200)
+// Date        : Sun Mar  1 14:42:33 2020
+// Host        : Steven-Win10-2 running 64-bit major release  (build 9200)
 // Command     : write_verilog -force -mode funcsim
-//               U:/ENSC452/dkong_system2/dkong_system.srcs/sources_1/bd/dkong/ip/dkong_framedoubler_slow_0_0/dkong_framedoubler_slow_0_0_sim_netlist.v
+//               C:/Users/Steven/Documents/HDL/vivado/dkong_system/dkong_system.srcs/sources_1/bd/dkong/ip/dkong_framedoubler_slow_0_0/dkong_framedoubler_slow_0_0_sim_netlist.v
 // Design      : dkong_framedoubler_slow_0_0
 // Purpose     : This verilog netlist is a functional simulation representation of the design and should not be modified
 //               or synthesized. This netlist cannot be used for SDF annotated simulation.
